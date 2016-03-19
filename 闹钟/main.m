29cae26ce769bf85e63e37896eee5b0b4f784604
gp <gp0120570@163.com>
2016-03-19 11:39:16 +0800

    Update main.m

--- 闹钟/main.m
+++ 闹钟/main.m
@@ -1,10 +1,6 @@
 //
 //  main.m
 //  闹钟
-//
-//  Created by 广州昂鼎信息科技有限公司 on 16/3/18.
-//  Copyright © 2016年 广州昂鼎信息科技有限公司. All rights reserved.
-//
 
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
